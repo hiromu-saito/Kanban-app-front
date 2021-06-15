b075dd9913e927a28126813ab8a1426bb54a4ff0
hiromu_saito <hiromu.saito@bigtreetc.com>
2021-06-15 23:01:07 +0900

    make guards.js and test

---
+++ test/unit/specs/router/guards.spec.js
@@ -0,0 +1,81 @@
+import {mount, createLocalVue} from '@vue/test-utils'
+import VueRouter from 'vue-router'
+
+import Vuex from 'vuex'
+
+const App = {
+  name: 'app',
+  render: h => h('router-view')
+}
+
+const Top = {
+  name: 'top',
+  render: h => h('p', ['top'])
+}
+
+const Login = {
+  name: 'login',
+  render: h => h('p', ['login'])
+}
+
+// storeのモック化
+const mockAuthorizeToken = store => {
+  const injector = require('inject-loader!@/router/guards')
+  const storeMock = injector({
+    '../store': store
+  })
+  return storeMock.authorizeToken
+}
+
+// Vueアプリのセットアップ
+const setup = state => {
+  // vuexストアの設定
+  const store = new Vuex.Store({state})
+
+  // routerの設定
+  const router = new VueRouter({
+    routes: [{
+      path: '/',
+      component: Top,
+      meta: {requiresAuth: true}
+    },
+    {
+      path: '/login',
+      component: Login
+    }]
+  })
+
+  router.beforeEach(mockAuthorizeToken(store))
+
+  // Appでマウントしてラッパーを返す
+  return mount(App, {
+    localVue,
+    store,
+    router
+  })
+}
+
+const localVue = createLocalVue()
+
+localVue.use(VueRouter)
+localVue.use(Vuex)
+
+describe('beforeEachガードフック', () => {
+  describe('認証トークンあり', () => {
+    it('そのまま解決すること', () => {
+      const app = setup({
+        auth: {
+          token: '1234567890abcdef',
+          userId: 1
+        }}
+      )
+      expect(app.text()).to.equal('top')
+    })
+  })
+  describe('認証トークンなし', () => {
+    it('/loginにリダイレクトすること', () => {
+      const app = setup({})
+      expect(app.text()).to.equal('login')
+    })
+  })
+})

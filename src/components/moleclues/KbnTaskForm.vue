935c56b36e65565fa3c4a693b25287a481ccefc9
hiromu_saito <hiromu.saito@bigtreetc.com>
2021-06-17 00:20:39 +0900

    add KbnTaskForm

---
+++ src/components/moleclues/KbnTaskForm.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <input
+      v-model="taskName"
+      type="text"
+      placeholder="タスク...">
+    <kbn-button
+      :disabled="disableAddTask"
+      @click.prevent="addTask">
+      追加
+    </kbn-button>
+    <kbn-button
+      @click.prevent="cancelTask">
+      キャンセル
+    </kbn-button>
+  </div>
+</template>
+
+<script>
+import KbnButton from '@/components/atoms/KbnButton'
+
+export default {
+  name: 'KbnTaskForm',
+  components: {
+    KbnButton
+  },
+  props: {
+    addTask: {
+      type: Function,
+      required: true
+    },
+    cancelTask: {
+      type: Function,
+      required: true
+    }
+  },
+  data () {
+    return {
+      taskName: ''
+    }
+  },
+  computed: {
+    disableAddTask () {
+      return this.taskName === ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ test/unit/specs/components/molecules/KbnTaskForm.spec.js
@@ -0,0 +1,29 @@
+import { mount } from '@vue/test-utils'
+import KbnTaskForm from '@/components/moleclues/KbnTaskForm'
+
+describe('KbnTaskForm', () => {
+  describe('disableAddTask', () => {
+    let taskForm
+    beforeEach(done => {
+      taskForm = mount(KbnTaskForm, {
+        propsData: {
+          addTask: () => {},
+          cancelTask: () => {}
+        }
+      })
+      taskForm.vm.$nextTick(done)
+    })
+    describe('入力なし', () => {
+      it('追加ボタンが押せないこと', () => {
+        taskForm.vm.$nextTick()
+        expect(taskForm.vm.disableAddTask).to.equal(true)
+      })
+    })
+    describe('入力あり', () => {
+      it('追加ボタンが押せること', () => {
+        taskForm.setData({taskName: 'task'})
+        expect(taskForm.vm.disableAddTask).to.equal(false)
+      })
+    })
+  })
+})

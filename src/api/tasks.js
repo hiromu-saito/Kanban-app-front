889749bd5a2a028e9674d6bb8a2c5022275ac7cd
hiromu_saito <hiromu.saito@bigtreetc.com>
2021-06-19 23:53:41 +0900

    add Task API

--- src/api/tasks.js
+++ src/api/tasks.js
@@ -9,5 +9,16 @@ export default {
           reject(new Error(err.response.data.message || err.message))
         })
     })
+  },
+  addTask: taskInfo => {
+    return new Promise((resolve, reject) => {
+      client.post('/tasks/add', taskInfo)
+        .then(res => {
+          resolve({id: res.data.id})
+        })
+        .catch(err => {
+          reject(new Error(err.response.data.message || err.message))
+        })
+    })
   }
 }
--- test/unit/specs/api/tasks.spec.js
+++ test/unit/specs/api/tasks.spec.js
@@ -8,22 +8,60 @@ const mockTasks = adapter => {
   return cilentMock.default
 }
 describe('TasksAPIモジュール', () => {
-  describe('失敗', () => {
-    it('エラーメッセージを取得できること', done => {
-      const message = 'failed removeTasks'
-      const adapter = config => {
-        return new Promise((resolve, reject) => {
-          const err = new Error(message)
-          err.response = {data: {message}, status: 401}
-          reject(err)
-        })
-      }
-      const tasksMock = mockTasks(adapter)
-      tasksMock.removeTask({id: 1, taskName: 'task'})
-        .catch(err => {
-          expect(err.message).to.equal(message)
-        })
-        .then(done)
+  describe('removeTask', () => {
+    describe('失敗', () => {
+      it('エラーメッセージを取得できること', done => {
+        const message = 'failed removeTasks'
+        const adapter = config => {
+          return new Promise((resolve, reject) => {
+            const err = new Error(message)
+            err.response = {data: {message}, status: 401}
+            reject(err)
+          })
+        }
+        const tasksMock = mockTasks(adapter)
+        tasksMock.removeTask({listId: 1, id: 1})
+          .catch(err => {
+            expect(err.message).to.equal(message)
+          })
+          .then(done)
+      })
+    })
+  })
+  describe('addTask', () => {
+    describe('成功', () => {
+      it('タスクIDが帰ってくること', (done) => {
+        const adapter = config => {
+          return new Promise((resolve, reject) => {
+            resolve({data: { id: 1 }, status: 200})
+          })
+        }
+        const tasksMock = mockTasks(adapter)
+
+        tasksMock.addTask({listId: 1, name: 'sample'})
+          .then(res => {
+            expect(res.id).to.equal(1)
+            done()
+          })
+      })
+    })
+    describe('失敗', () => {
+      it('エラーメッセージを取得できること', done => {
+        const message = 'failed addTask'
+        const adapter = config => {
+          return new Promise((resolve, reject) => {
+            const err = new Error(message)
+            err.response = {data: {message}}
+            reject(err)
+          })
+        }
+        const tasksMock = mockTasks(adapter)
+        tasksMock.addTask({listId: 1, name: 'sample'})
+          .catch(err => {
+            expect(err.message).to.equal('failed addTask')
+            done()
+          })
+      })
     })
   })
 })

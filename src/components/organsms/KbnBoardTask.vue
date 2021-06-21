2e3f41ef0ae0952c3855e5620462bacb5590fd93
hiromu_saito <hiromu.saito@bigtreetc.com>
2021-06-22 08:59:15 +0900

    WIP Kbn BoardTask

---
+++ src/components/organsms/KbnBoardTask.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <section>
+      <kbn-task-list
+        v-for="list in lists"
+        :key="list.id"
+        :list="list"/>
+    </section>
+  </div>
+</template>
+
+<script>
+import KbnTaskList from './KbnTaskList'
+export default {
+  name: 'KbnBoardTask',
+  components: { KbnTaskList },
+  data () {
+    return {
+      lists: []
+    }
+  },
+  mounted () {
+    // this.$store.dispatch('fetchList')
+    //   .then(res => {
+    //     this.lists = res.lists
+    //   })
+    //   .catch(err => {
+    //     throw err
+    //   })
+    this.lists = [
+      {id: 1, name: 'TODO', items: [{id: 1, name: 'task', description: 'description'}]},
+      {id: 2, name: 'WIP', items: [{id: 1, name: 'task', description: 'description'}]},
+      {id: 3, name: 'DONE', items: [{id: 1, name: 'task', description: 'description'}]}
+    ]
+  }
+}
+</script>
+
+<style scoped>
+section{
+  display: flex;
+  justify-content: space-around;
+}
+
+</style>
--- src/components/templates/KbnBoardView.vue
+++ src/components/templates/KbnBoardView.vue
@@ -1,17 +1,22 @@
 <template>
-  <p>ボードページ</p>
+  <div>
+    <kbn-board-task/>
+  </div>
 </template>
 
 <script>
-/* eslint-disable */
-export default {
-  name: 'KbnBoardView',
+import KbnBoardTask from '../organsms/KbnBoardTask'
 
+/* eslint-disable */
+export default{
+  name: 'KbnBoardView',
+  components: { KbnBoardTask },
   // /* NOTE: ErrorBoundary の動作確認ためのコード
-  created(){
-    throw new Error('レンダリングに失敗しました！')
-  }
+  // created(){
+  //   throw new Error('レンダリングに失敗しました！')
+  // }
   // */
 }
+
 /* eslint-enable */
 </script>
